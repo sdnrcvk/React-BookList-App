3ea2f80a3dc4e886d8c7477cc3978290b5374b95
Sedanur Çevik <sdnrcvk@gmail.com>
2023-02-12 01:07:25 +0300

    BookList component edited

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function BookDetails() {
+export default function BookDetails({book}) {
   return (
-    <div>
-        
-    </div>
+    <li>
+        <div className='title'>{book.ad}</div>
+        <div className='author'>{book.yazar}</div>
+    </li>
   )
 }
