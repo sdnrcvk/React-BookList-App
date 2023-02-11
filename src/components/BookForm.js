60618e37f1c10ee1fcd71e0b52e87b2c113505d1
Sedanur Çevik <sdnrcvk@gmail.com>
2023-02-12 02:14:47 +0300

    BookForm component created

---
+++ src/components/BookForm.js
@@ -0,0 +1,26 @@
+import React,{useContext,useState} from 'react'
+import { BookContext } from '../contexts/bookContext'
+
+export default function BookForm() {
+
+  const {addBook}=useContext(BookContext);
+  const [title,setTitle]=useState("");
+  const [author,setAuthor]=useState("");
+
+  const handleSubmit=(e)=>{
+    e.preventDefault(); //varsayılan özelliklerini yok edelim
+    //console.log(title,author);
+
+    addBook(title,author);
+    setTitle("");
+    setAuthor("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Kitap Adı' value={title} onChange={(e)=>setTitle(e.target.value)}/>
+        <input type="text" placeholder='Yazar Adı' value={author} onChange={(e)=>setAuthor(e.target.value)}/>
+        <input type="submit" value="Kitap Ekle" />
+    </form>
+  )
+}
