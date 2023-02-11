a8487fb76b75eef4da1e4f30a4429ef272ca53bf
Sedanur Çevik <sdnrcvk@gmail.com>
2023-02-12 01:55:36 +0300

    Delete button added and css changed

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { BookContext } from '../contexts/bookContext'
 
 export default function BookDetails({book}) {
+
+  const {removeBook}=useContext(BookContext);
+
   return (
     <li>
         <div className='title'>{book.ad}</div>
         <div className='author'>{book.yazar}</div>
+        <div style={{textAlign:"end"}}>
+            <button type='button' onClick={()=>removeBook(book.id)}>Sil</button>
+        </div>
     </li>
   )
 }
